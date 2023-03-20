4e58e54b36701e63e5feb9997e046b4326a0205d
Jeniffer Mendes <115995202+JenifferMendes@users.noreply.github.com>
2023-03-20 13:30:53 -0300

    uptade: criação do segundo objeto(imutável)

--- dia2/src/immutable.js
+++ dia2/src/immutable.js
@@ -5,10 +5,13 @@ const john = {
   hobbies: ['Surf', 'Design'],
 }
 
-const jane = john
+const jane = {
+  ...john,
+  name: 'Jane',
+  hobbies: [...john.hobbies, 'MuayThai', 'Programming']
+}
 
-jane.name = 'Jane'
-jane.hobbies.push('MuayThai', 'Programming')
 
 console.log('John:', john)
 console.log('Jane:', jane)
+console.log(john === jane)
